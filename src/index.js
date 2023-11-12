e40d65ea8d568ab4f37f93d747b25033c412a246
DESKTOP-S410I7S\Purushothaman <Purushothaman R>
2023-11-12 14:11:39 +0530

    code before deploy to netlify

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import { BrowserRouter } from "react-router-dom";
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { CategoriesProvider } from './contexts/categories.context';
+import { CartProvider } from './contexts/cart.context';
 
-import App from "./App";
-import { UserProvider } from "./context/user.context";
-import { ProductsProvider } from "./context/productes.context";
-import { CartProvider } from "./context/cart.context";
+import './index.scss';
 
-import "./index.scss";
+const rootElement = document.getElementById('root');
 
-import reportWebVitals from "./reportWebVitals";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
